79beb66872901b6912faa0c3e88efa66d265eea6
shenjies88 <1961486254@qq.com>
2021-05-21 22:48:21 +0800

    fix:限流降级处理

--- shop-order/src/main/java/com/heima/controller/SentinelOrderController.java
+++ shop-order/src/main/java/com/heima/controller/SentinelOrderController.java
@@ -1,5 +1,7 @@
 package com.heima.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.heima.service.SentinelOrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +33,12 @@ public class SentinelOrderController {
     }
 
     int i = 0;
+
+    @SentinelResource(
+            value = "message2",
+            blockHandler = "blockHandler",
+            fallback = "fallback"
+    )
     @GetMapping("/order/message2")
     public String message2() {
         i++;
@@ -38,5 +46,15 @@ public class SentinelOrderController {
             throw new RuntimeException();
         }
         return "message2";
+    }
+
+    public String blockHandler(BlockException ex) {
+        log.error("", ex);
+        return "接口被限流或者降级了...";
+    }
+
+    public String fallback(Throwable throwable) {
+        log.error("", throwable);
+        return "接口发生异常了...";
     }
 }
