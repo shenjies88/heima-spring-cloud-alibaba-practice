6b1446777a9a9c5baf0b82a598c857d8d7a9799c
shenjies88 <1961486254@qq.com>
2021-05-21 23:52:32 +0800

    fix:网关转发

---
+++ api-gateway/src/main/java/com/heima/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.heima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shenjies88
+ * @since 2021/5/21-11:30 下午
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
