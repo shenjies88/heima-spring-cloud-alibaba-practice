4a1c9d7a6e71e7c34a60bae4405713b4c2bb00aa
shenjies88 <1961486254@qq.com>
2021-05-22 23:06:02 +0800

    fix:gateway和sentinel集成有问题

--- api-gateway/src/main/java/com/heima/AuthGlobalFilter.java
+++ api-gateway/src/main/java/com/heima/AuthGlobalFilter.java
@@ -1,37 +1,50 @@
-//package com.heima;
-//
-//import com.alibaba.cloud.commons.lang.StringUtils;
-//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-//import org.springframework.cloud.gateway.filter.GlobalFilter;
-//import org.springframework.core.Ordered;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.server.ServerWebExchange;
-//import reactor.core.publisher.Mono;
-//
-///**
-// * @author shenjies88
-// * @since 2021/5/21-11:57 下午
-// */
-//@Component
-//public class AuthGlobalFilter implements GlobalFilter, Ordered {
-//    //完成判断逻辑
-//    @Override
-//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain
-//            chain) {
-//        String token = exchange.getRequest().getQueryParams().getFirst("token");
-//        if (StringUtils.isBlank(token)) {
-//            System.out.println("鉴权失败");
-//            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-//            return exchange.getResponse().setComplete();
-//        }
-//        //调用chain.filter继续向下游执行
-//        return chain.filter(exchange);
-//    }
-//
-//    //顺序,数值越小,优先级越高 @Override
-//    @Override
-//    public int getOrder() {
-//        return 0;
-//    }
-//}
+package com.heima;
+
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.alibaba.fastjson.JSON;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author shenjies88
+ * @since 2021/5/21-11:57 下午
+ */
+@Component
+public class AuthGlobalFilter implements GlobalFilter, Ordered {
+
+    /**
+     * 完成判断逻辑
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getQueryParams().getFirst("token");
+        if (StringUtils.isBlank(token)) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("code", 1);
+            map.put("message", "接口无权限");
+            DataBuffer dataBuffer = exchange.getResponse().bufferFactory().wrap(JSON.toJSONString(map).getBytes());
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().writeWith(Flux.just(dataBuffer));
+        }
+        //调用chain.filter继续向下游执行
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 顺序,数值越小,优先级越高
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ api-gateway/src/main/java/com/heima/GatewayConfiguration.java
@@ -0,0 +1,90 @@
+package com.heima;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
+import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+/**
+ * @author shenjies88
+ * @since 2021/5/22-9:11 下午
+ */
+@Configuration
+public class GatewayConfiguration {
+
+    private final List<ViewResolver> viewResolvers;
+    private final ServerCodecConfigurer serverCodecConfigurer;
+
+    @Value("${spring.cloud.nacos.discovery.server-addr}")
+    private String nacosAddress;
+    @Value("${spring.cloud.sentinel.datasource.gateway-flow-ds.nacos.groupId}")
+    private String groupId;
+    @Value("${spring.cloud.sentinel.datasource.gateway-flow-ds.nacos.dataId}")
+    private String dataId;
+
+    public GatewayConfiguration(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+                                ServerCodecConfigurer serverCodecConfigurer) {
+        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        this.serverCodecConfigurer = serverCodecConfigurer;
+    }
+
+    /**
+     * 限流配置
+     */
+    @PostConstruct
+    public void initGatewayRules() {
+        ReadableDataSource<String, Set<GatewayFlowRule>> dataSource = new NacosDataSource<>(nacosAddress, groupId, dataId,
+                source -> JSON.parseObject(source, new TypeReference<Set<GatewayFlowRule>>() {
+                }));
+        GatewayRuleManager.register2Property(dataSource.getProperty());
+    }
+
+    /**
+     * 配置限流的异常处理器
+     */
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelGatewayBlockExceptionHandler
+    sentinelGatewayBlockExceptionHandler() {
+        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+    }
+
+    /**
+     * 自定义限流异常页面
+     */
+    @PostConstruct
+    public void initBlockHandlers() {
+        BlockRequestHandler blockRequestHandler = (serverWebExchange, throwable) -> {
+            Map<String, Object> map = new HashMap<>();
+            map.put("code", 1);
+            map.put("message", "接口被限流了");
+            return ServerResponse
+                    .status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(BodyInserters.fromValue(map));
+        };
+        GatewayCallbackManager.setBlockHandler(blockRequestHandler);
+    }
+
+
+}
